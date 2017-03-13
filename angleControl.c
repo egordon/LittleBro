1bbcd2c928d0526835a36a8808072d5591b97a70
dcw3 <dcw3@princeton.edu>
2017-03-13 18:44:59 -0400

    file

---
+++ angleControl.c
@@ -0,0 +1,10 @@
+/***********************************************
+Filename: angleControl.c
+Author: Changyan
+Date: 3/11/17
+***********************************************/
+
+#include <matrix.h>
+#include <stdio.h>
+#include <stdlib.h>
+
