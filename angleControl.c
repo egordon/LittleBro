686b4c3ef13e51523b657ca3cf0a1bfdf907eaab
dcw3 <dcw3@princeton.edu>
2017-03-13 18:46:49 -0400

    f

--- angleControl.c
+++ angleControl.c
@@ -7,4 +7,25 @@ Date: 3/11/17
 #include <matrix.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
+#include <pigpio.h>
+
+static double homeAngle = 0; /* desired angle */
+static VEC *angleState;
+
+/* functions to read input from the gyroscope and compass */
+
+
+
+/* function to run Kalman filter, given the previous state */
+AngleState_T Kalman(AngleState_T prev, ) {
+
+}
+
+/* function to return the correct voltage that should be
+	sent to the right motor, given the current angle*/
+int rightMotorVoltage(double currentAngle) {
+
+}
