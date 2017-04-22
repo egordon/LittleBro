e8321a161e02777bc89bfc743b784d14ee91c7b7
Pave <Daniel@Stanley5.com>
2017-04-21 21:00:33 -0400

    Changed i2ctest.c

--- testi2c.c
+++ testi2c.c
@@ -1,5 +1,6 @@
 #include <pigpiod_if2.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
   int pi = pigpio_start(NULL, NULL);
@@ -12,9 +13,9 @@ int main() {
   int CTRL4 = 0x23;
   int LOWODR = 0x39;
   // these init values are taken from the pololu library
-  i2c_write_byte_data(pi, handleA, CTRL1, 0x6F);
-  i2c_write_byte_data(pi, handleA, CTRL1, 0x00);
-  i2c_write_byte_data(pi, handleA, CTRL1, 0x00);
+  i2c_write_byte_data(pi, handleA, CTRL1, 0x0F); // pololu has 0x6f
+  i2c_write_byte_data(pi, handleA, CTRL4, 0x00); // highest resolution
+  i2c_write_byte_data(pi, handleA, LOWODR, 0x00);
 
 
 
@@ -24,12 +25,27 @@ int main() {
   while (1) {
     int i = 0;
 
+    unsigned char buffer[32];
+    buffer[6] = 42; // Sentinel to check whether it's being touched
+    sleep(1);
+    // yes, I think it's strange we're writing to the output register
+    i2c_write_byte_data(pi, handleA, regaddr, 0x80);
+    sleep(1);
+    i2c_read_block_data(pi, handleA, regaddr, buffer);
+
+    for(i=0;i<7;i++){
+      printf("%d: \t%d\n", i, buffer[i]);
+    }
+    printf("\n");
+    
+    /*
     for(i=0; i<10; i++){
   	data = i2c_read_byte_data(pi, handleA, regaddr+i);
   	printf("A, %x: data is %d = %x\n", regaddr+i, data, regaddr+i);
   	time_sleep(.1);
     }
     printf("\n");
+    */
   }
   //handle = i2c_close(pi, bus);
 
